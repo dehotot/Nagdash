c012394b98739d8b4bebd55d349fb2a54bf412e9
Alex White <alex.white@diamond.ac.uk>
2020-03-10 15:44:22 +0000

    run nagdash inside a Docker container

---
+++ container/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# create the config file from template
+sed "s/%NAGIOS_API_HOST%/${NAGIOS_API_HOST}/g; s/%NAGIOS_API_PORT%/${NAGIOS_API_PORT}/g; " /var/www/html/Nagdash/config.php.template \
+	> /var/www/html/Nagdash/config.php
+
+echo '== Nagdash Config ============================================================'
+cat /var/www/html/Nagdash/config.php
+echo ''
+echo '=============================================================================='
+echo ''
+
+# start the webserver
+touch /var/log/httpd/access_log /var/log/httpd/error_log
+httpd
+tail -f /var/log/httpd/access_log >> /dev/stdout &
+tail -f /var/log/httpd/error_log >> /dev/stderr
